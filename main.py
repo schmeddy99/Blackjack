09dd704650f6bcd061f08a28bac1c0d2e47a4eae
schmeddy99 <ahmedaosman00@gmail.com>
2025-03-12 06:25:55 -0700

    completion of mvp

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 isActive = True
 
-
-
-
 def main():
     user_cards = []
     computer_cards = []
@@ -21,8 +18,8 @@ def main():
         user_total += card
     print(f"Your cards: {user_cards}, current score: {user_total}")
 
-    computer_cards = random.choice(cards)
-    computer_total += computer_cards
+    computer_cards = [random.choice(cards)]
+    computer_total = sum(computer_cards)
     print(f"Computer's cards: {computer_cards}, computer's score: {computer_total}")
 
     ## type y to continue n to pass
@@ -34,11 +31,24 @@ def main():
         print(f"Your cards: {user_cards}, current score: {user_total}")
 
     
+    while computer_total < 17:
+        new_computer_card = random.choice(cards)
+        computer_cards.append(new_computer_card)
+        computer_total += new_computer_card
+        print(
+            f"Computer drew: {new_computer_card}. Computer's cards: {computer_cards}, current score: {computer_total}")
+        
+    if user_total > 21:
+        print("You went over. You lose.")
+    elif computer_total > 21:
+        print("Computer went over. You win.")
+    elif user_total == computer_total:
+        print("It's a draw.")
+    elif user_total > computer_total:
+        print("You win.")
+    else:
+        print("You lose.")
 
-    ## if y add another card, calculate total
-    ## if n reveal computers other card, is less than 17? hit
-    ## repeat until > 17 or < 21
-    ## compare scores decide winner
 
 while isActive:
     continue_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
@@ -47,3 +57,5 @@ while isActive:
         main()
     else:
         isActive = False
+
+
